11736481908d5b8ada46dda137d53185dc31d5f8
ValezaL <valzalipaj@gmail.com>
2024-01-27 08:53:49 +0100

    CSS

--- TrimTrim/obj/Debug/net7.0/TrimTrim.AssemblyInfo.cs
+++ TrimTrim/obj/Debug/net7.0/TrimTrim.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrimTrim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7917afab1837a424c81b9fba99b8c9653d49e7a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrimTrim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrimTrim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
